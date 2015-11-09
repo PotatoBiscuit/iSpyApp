805648a1502cd7060a80df1cc5dbddcc7e4c62ee
Erik <eld66@nau.edu>
2015-11-08 18:22:59 -0700

    Current Project
    I just added what I currently have done for the project. It is set up
    like a tree hierarchy, with the MainPage as a parent for all the other
    pages.

---
+++ ISpy/app/src/main/java/net/androidbootcamp/ispy/AddDeleteEvents.java
@@ -0,0 +1,40 @@
+package net.androidbootcamp.ispy;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddDeleteEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_delete_events);
+
+        if(getActionBar() != null){
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_delete_events, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
